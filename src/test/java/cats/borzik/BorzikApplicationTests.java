69a087d1c12d519e962358e8176de2b60b61086d
grbm <olejnec@gmail.com>
2025-02-05 15:21:23 +0300

    add front

--- src/test/java/cats/borzik/BorzikApplicationTests.java
+++ src/test/java/cats/borzik/BorzikApplicationTests.java
@@ -1,13 +1,5 @@
 package cats.borzik;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class BorzikApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
